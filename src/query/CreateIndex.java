860f770bac9beec50f59770de2383fb9ef7a3cc1
Hyun Chai Jeong <jeong3@purdue.edu>
2015-12-05 09:03:33 -0500

    create/delete index and table

---
+++ src/query/CreateIndex.java
@@ -0,0 +1,51 @@
+package query;
+
+import global.Minibase;
+import index.HashIndex;
+import parser.AST_CreateIndex;
+import parser.ParseException;
+import relop.Schema;
+
+
+/**
+ * Execution plan for creating indexes.
+ */
+class CreateIndex implements Plan {
+
+	protected String fileName;
+	protected String iColumn;
+	protected String iTable;
+
+  protected Schema schema;
+
+
+  /**
+   * Optimizes the plan, given the parsed query.
+   * 
+   * @throws QueryException if index already exists or table/column invalid
+   */
+  public CreateIndex(AST_CreateIndex tree) throws QueryException {
+
+		fileName = tree.getFileName();
+    QueryCheck.fileNotExists(fileName);
+//    QueryCheck.indexExists(fileName);
+
+		iColumn = tree.getIxColumn();
+//		QueryCheck.columnExists(schema, iColumn);
+		
+		iTable = tree.getIxTable();
+		QueryCheck.tableExists(iTable);
+  }
+
+  /**
+   * Executes the plan and prints applicable output.
+   */
+  public void execute() {
+
+    new HashIndex(fileName);
+System.out.println("index to create: " + fileName);
+    Minibase.SystemCatalog.createIndex(fileName, iTable, iColumn);
+
+		System.out.println("Index created.");
+	}
+}
---
+++ src/query/DropTable.java
@@ -0,0 +1,52 @@
+package query;
+
+import global.Minibase;
+import heap.HeapFile;
+import index.HashIndex;
+import parser.AST_DropTable;
+
+/**
+ * Execution plan for dropping tables.
+ */
+class DropTable implements Plan {
+
+  protected String fileName;
+
+
+  /**
+   * Optimizes the plan, given the parsed query.
+   * 
+   * @throws QueryException if table doesn't exist
+   */
+  public DropTable(AST_DropTable tree) throws QueryException {
+
+    fileName = tree.getFileName();
+    QueryCheck.tableExists(fileName);
+
+  }
+
+  /**
+   * Executes the plan and prints applicable output.
+   */
+  public void execute() {
+
+    IndexDesc[] inds = Minibase.SystemCatalog.getIndexes(fileName);
+System.out.println("table to drop: " + fileName);
+    for (IndexDesc ind : inds) {
+System.out.println("index to delete: " + ind.indexName);
+}
+    for (IndexDesc ind : inds) {
+//      new HashIndex(ind.indexName).deleteFile();
+//      HashIndex h = new HashIndex(ind.indexName);
+//			h.deleteFile();
+      Minibase.SystemCatalog.dropIndex(ind.indexName);
+    }
+
+//    new HeapFile(fileName).deleteFile();
+    Minibase.SystemCatalog.dropTable(fileName);
+
+    System.out.println("Table dropped.");
+
+  }
+
+}
